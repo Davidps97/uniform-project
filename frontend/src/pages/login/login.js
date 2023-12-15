0961f832ac947f7d4f295686efeb38f3efa9f9a2
David <davidperezsantana1997@gmail.com>
2023-12-15 17:31:13 +0000

    readme

--- frontend/src/pages/login/login.js
+++ frontend/src/pages/login/login.js
@@ -1,33 +1,48 @@
-import "./login.css";
+import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useRef } from "react";
 import { login } from "../../services/auth";
 import { jwtDecode } from "jwt-decode";
 import { getRoleWithUser } from "../../services/role.service";
 import { getOneUser } from "../../services/user.service";
+import "./login.css";
 
 function Login() {
-  const loginRef = useRef() 
+  const [error, setError] = useState("");
+  const loginRef = useRef();
   const navigate = useNavigate();
-    const handleLogin = (e) => {
+
+  const handleLogin = (e) => {
     e.preventDefault();
     const email = loginRef.current.email.value;
     const password = loginRef.current.password.value;
 
-    login(email, password).then(response => {
-      const decodeToken = jwtDecode(response.data.token);
-      getRoleWithUser(decodeToken.sub).then(response => {
-        localStorage.setItem("role", response.data.assigned_roles.name);
-      }) 
+    login(email, password)
+      .then(({ data: { token } }) => {
+        const decodeToken = jwtDecode(token);
 
-      getOneUser(decodeToken.sub).then(response => {
-        localStorage.setItem("user", JSON.stringify(response.data));
+        getRoleWithUser(decodeToken.sub)
+          .then((roleResponse) => {
+            localStorage.setItem("role", roleResponse.data.assigned_roles.name);
+          })
+          .catch((roleError) => {
+            setError("Error retrieving user role");
+          });
+
+        getOneUser(decodeToken.sub)
+          .then((userResponse) => {
+            localStorage.setItem("user", JSON.stringify(userResponse.data));
+          })
+          .catch((userError) => {
+            setError("Error retrieving user information");
+          });
+
+        localStorage.setItem("token", token);
+        navigate("/users");
       })
-
-      localStorage.setItem("token", response.data.token);
-      navigate("/users");
-    })
-  }
+      .catch((loginError) => {
+        setError("Invalid email or password");
+      });
+  };
 
   return (
     <div className="login-container">
@@ -35,15 +50,28 @@ function Login() {
       <div className="input-container">
         <div className="form-container">
           <p className="title">LOGIN</p>
-          <form className="form" ref={loginRef} onSubmit={(e) => handleLogin(e)}>
-            <input name="email" type="email" class="input" placeholder="Email" />
-            <input name="password" type="password" class="input" placeholder="Password" />
+          <form
+            className="form"
+            ref={loginRef}
+            onSubmit={(e) => handleLogin(e)}
+          >
+            <input
+              name="email"
+              type="email"
+              className="input"
+              placeholder="Email"
+            />
+            <input
+              name="password"
+              type="password"
+              className="input"
+              placeholder="Password"
+            />
+            {error && <p className="error-message">{error}</p>}
             <p className="page-link">
               <span className="page-link-label">Forgot Password?</span>
             </p>
-            <button className="form-btn">
-              LOG IN
-            </button>
+            <button className="form-btn">LOG IN</button>
           </form>
         </div>
       </div>
